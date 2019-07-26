3652b998c4414f2d883f894e07875d52b332ff1e
Joo-Ho Lee <jooho_lee@outlook.kr>
2019-07-26 12:59:34 +0900

    ENH: Update pypi development status

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="random_group_builder",
+    name="test",
     version="0.1.0",
     description="Help you to build groups from some data randomly.",
     long_description=open('README.md').read(),
@@ -11,10 +11,11 @@ setup(
 
     install_requires=[],
     packages="random_group_builder",
-    keywords=['random', 'group', 'builder', 'helper', 'api'],
+    keywords=['random', 'group', 'builder', 'helper', 'api', 'rgb'],
     python_requires='>=3',
     zip_safe=False,
     classifiers=[
+        'Development Status :: 1 - Planning',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
